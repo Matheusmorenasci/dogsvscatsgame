39a5d3f0cb609e1594448157a9f2168653274bb4
DevSP <contatodevsp@gmail.com>
2021-01-07 21:30:02 -0300

    Mostrando score ao atualizar a pagina

--- scripts/game.js
+++ scripts/game.js
@@ -11,23 +11,21 @@ var winner = ["Dogs", "Cats"];
 
 var winStates = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-localStorage.setItem("dogScore", 0);
-localStorage.setItem("catScore", 0);
+// Essa linhas estão limpando o score sempre que a pagina carrega, 
+// vc vai precisa criar um botão para limpar o score com essas linhas.
+// localStorage.setItem("dogScore", 0);
+// localStorage.setItem("catScore", 0);
 
 function handleMove(position) {
 
-    if (gameOver) {
-        return;
-    }
+    if (gameOver) return;
 
     if (board[position] == '') {
         board[position] = symbols[playerTime];
 
         gameOver = isWinOrTie();
 
-        if (!gameOver) {
-            playerTime = playerTime == 0 ? 1 : 0;
-        }
+        if (!gameOver) playerTime = (playerTime == 0) ? 1 : 0;
 
         return gameOver;
     }
@@ -46,10 +44,6 @@ function isWinOrTie() {
         if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != '') {
             return true;
         }
-    }
-
-    function isFilled(space) {
-        return space != '';
     }
 
     if (board.every(function (space) {
